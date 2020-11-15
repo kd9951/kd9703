0f95c209a8c0e3e9098335f5fe865affe69582b8
harada <kd9951@gmail.com>
2020-11-16 05:00:08 +0900

    修正 不要コード

--- routes/console.php
+++ routes/console.php
@@ -1,25 +1,14 @@
 <?php
 
-use Crawler\HttpClients\TwitterApi;
 use Illuminate\Support\Facades\Artisan;
-use Kd9703\Constants\Media;
-use Kd9703\Entities\Media\Account;
 
 /**
  * ユーザー情報を更新
  */
 Artisan::command('app:update-users {limit_sec=50}', function (
-    \Kd9703\Resources\Interfaces\Account\Account $Account,
-    \Kd9703\MediaBinder $MediaBinder
+    \Kd9703\Usecases\UpdateUsers $UpdateUsers
 ) {
-    $account = $Account->getOne(Media::TWITTER(), config('services.twitter.owner_twitter_id'));
-
-    $MediaBinder->bind($account);
-
-    $UpdateUsers = app(\Kd9703\Usecases\UpdateUsers::class);
-
     $result = ($UpdateUsers)([
         'limit_sec' => $this->argument('limit_sec'),
     ]);
 });
-
