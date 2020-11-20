0e6ae8f65ea5791bddb5369209e27c3f2bcfb4be
harada <kd9951@gmail.com>
2020-11-21 06:56:03 +0900

    修復 分離不足

--- kd9703/MediaAccess/Twitter/GetUsers.php
+++ kd9703/MediaAccess/Twitter/GetUsers.php
@@ -2,7 +2,6 @@
 namespace Kd9703\MediaAccess\Twitter;
 
 use Carbon\Carbon;
-use Kd9703\Constants\Prefecture;
 use Kd9703\Entities\Media\Account;
 use Kd9703\Entities\Media\Accounts;
 use Kd9703\MediaAccess\Interfaces\GetUsers as GetUsersInterface;
@@ -72,7 +71,7 @@ class GetUsers extends MediaAccess implements GetUsersInterface
 
         foreach ($response_json_array as $user) {
 
-            $account    = $this->FormatUserObjectForAccount($user);
+            $account = $this->FormatUserObjectForAccount($user);
 
             $account_id = $account['account_id'];
             unset($account['account_id']);
@@ -83,98 +82,4 @@ class GetUsers extends MediaAccess implements GetUsersInterface
         return $formatted;
     }
 
-    /**
-     * @param string $str
-     */
-    protected function guessPrefecture(string $name, string $description, string $location): ?int
-    {
-        // 推定の優先度 場所→名前(人名っぽいものは省いて)→紹介文
-        return $this->seekPrefecture($location) ?:
-        // 自己紹介文には「サッカーの香川選手が好きです」と書くかもしれないので
-        ($this->seekPrefectureStrict($name) ?: $this->seekPrefectureStrict($description));
-    }
-
-    /**
-     * @param string $str
-     */
-    protected function seekPrefecture(string $str): ?int
-    {
-        foreach (Prefecture::TEXT_JPN as $id => $name) {
-            if (strpos($str, $name) !== false) {
-                return $id;
-            }
-        }
-
-        foreach (Prefecture::TEXT_EN as $id => $name) {
-            if (strpos($str, $name) !== false) {
-                return $id;
-            }
-        }
-
-        foreach (Prefecture::TEXT_JPN_SHORT as $id => $name) {
-            if (strpos($str, $name) !== false) {
-                return $id;
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * 人名かもしれない都道府県名はマッチさせない
-     * @param string $str
-     */
-    protected function seekPrefectureStrict(string $str): ?int
-    {
-        // 名字由来net調べ
-        $strict_id = [
-            3, //岩手さん    390人
-            8, //茨城さん    340人
-            // 9,  //栃木さん    4000人
-            10, //群馬さん    40人
-            11, //埼玉さん    30人
-            13, //東京さん    10人
-            14, //神奈川さん  190人
-            15, //新潟さん    340人
-            // 19, //山梨さん    5800人
-            21, //岐阜さん    20人
-            22, //静岡さん    210人
-            23, //愛知さん    870人
-            24, //三重さん    560人
-            25, //滋賀さん    180人
-            26, //京都さん     90人
-            27, //大阪さん    1200人
-            28, //兵庫さん    1800人
-            30, //和歌山さん  240人
-            31, //鳥取さん    1700人
-            // 32, //島根さん    4900人
-            // 34, //広島さん    6300人
-            38, //愛媛さん    0人
-            39, //高知さん    280人
-            // 41, //佐賀さん    6500人
-            44, //大分さん    30人
-            46, //鹿児島さん  1000人
-            47, //沖縄さん    0人
-        ];
-        foreach (Prefecture::TEXT_JPN as $id => $name) {
-            if (strpos($str, $name) !== false) {
-                return $id;
-            }
-        }
-
-        foreach (Prefecture::TEXT_EN as $id => $name) {
-            // osaka 大坂さんや小坂さんは多いので
-            if ($id !== 27 && in_array($id, $strict_id) && strpos($str, $name) !== false) {
-                return $id;
-            }
-        }
-
-        foreach (Prefecture::TEXT_JPN_SHORT as $id => $name) {
-            if (in_array($id, $strict_id) && strpos($str, $name) !== false) {
-                return $id;
-            }
-        }
-
-        return null;
-    }
 }
